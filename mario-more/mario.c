8a4b571a49c2bc23649705b2ace7235fac5d595a
Somesh Kar <somesh.kar@gmail.com>
2019-11-17 12:38:12 +0000

    fix style50 errors

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,35 +1,57 @@
 #include <cs50.h>
+
 #include <stdio.h>
 
 int main(void)
 {
     int height;
 
-    do {
+    // start loop until user gives correct inputs
+    do
+    {
         height = get_int("Height: ");
     }
     while (height < 1 || height > 8);
 
-    if (height > 0 || height < 9) {
+    // check for height being in the correct limits
+    if (height > 0 || height < 9)
+    {
         int counter = 0;
-        for (int row=0; row<height; row++) {
-            if (counter != height) {
-                for (int spaces = (height-1) - counter; spaces > 0; spaces--) {
+
+        // loop over blocks of hashes
+        for (int row = 0; row < height; row++)
+        {
+            // make sure you haven't reached the required height already
+            if (counter != height)
+            {
+
+                // print appropriate spaces
+                for (int spaces = (height - 1) - counter; spaces > 0; spaces--)
+                {
                     printf(" ");
                 }
-                for (int hashes = 0; hashes <= counter; hashes++) {
+
+                // print first half of hashes
+                for (int hashes = 0; hashes <= counter; hashes++)
+                {
                     printf("#");
                 }
 
+                // print gap between hash sections
                 printf("  ");
 
-                for (int hashes = 0; hashes <= counter; hashes++) {
+                // print second half of hashes
+                for (int hashes = 0; hashes <= counter; hashes++)
+                {
                     printf("#");
                 }
 
+                // go to the next line to start again
                 printf("\n");
+
+                // increment the counter
                 counter++;
             }
-        }     
-    }       
+        }
+    }
 }
